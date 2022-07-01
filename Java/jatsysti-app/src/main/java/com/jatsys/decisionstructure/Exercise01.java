09601d12bdbc2778d08d9af64548eacb4163109d
jatjms <jhonatanat@gmail.com>
2022-07-01 20:32:15 -0300

    Exercitando JAVA

---
+++ Java/jatsysti-app/src/main/java/com/jatsys/decisionstructure/Exercise01.java
@@ -0,0 +1,24 @@
+package com.jatsys.decisionstructure;
+
+import java.util.Scanner;
+public class Exercise01 {
+	// 1. Faça um Programa que peça dois números e imprima o maior deles.
+	public static void main(String[] args) {
+		try(Scanner scan = new Scanner(System.in)){
+			 int num1, num2;
+			 
+			 System.out.println("Digite o 1º número.");
+			 num1 = scan.nextInt();
+			 System.out.println("Digite o 2º número.");
+			 num2 = scan.nextInt();
+			
+			 if(num1>num2) {
+				 System.out.print("o "+num1+", 1º número é maior que o 2º.");
+			 }else {
+				 System.out.print("o "+num2+", 2º número é maior que o 1º.");
+			 }
+				 
+		}
+	}
+
+}
